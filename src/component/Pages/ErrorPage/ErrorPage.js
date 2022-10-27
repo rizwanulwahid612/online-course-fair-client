dde1a8a818c415cb76f7c6a214d4689cd75ab174
Rizwanul Wahid <rizwanulwahid612@gmail.com>
2022-10-28 03:50:55 +0600

    error page

--- src/component/Pages/ErrorPage/ErrorPage.js
+++ src/component/Pages/ErrorPage/ErrorPage.js
@@ -9,6 +9,7 @@ const ErrorPage = () => {
              <div style={{marginTop:'100px'}} className='App'>
              <div>
              <h1>This page is not found. please go back to home.</h1>
+             <h3>Please write the currect url</h3>
               <h4>Error Page </h4>
              </div>
              
